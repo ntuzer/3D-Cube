ed097e6de2845821f7d595402efdb76871f29ad8
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-02-15 14:41:02 -0800

    Get face positions of cube using raycasting

--- js/rotation.js
+++ js/rotation.js
@@ -29,17 +29,23 @@ Defmech.RotationWithQuaternion = (function()
 			cube24, cube25, cube26;
 
 	var mouseDown = false;
+	var projector;
+	var mouse = new THREE.Vector2();
 	var rotateStartPoint = new THREE.Vector3(0, 0, 1);
 	var rotateEndPoint = new THREE.Vector3(0, 0, 1);
 
-	var curQuaternion;
+	var curQuaternion, cur1Quaternion;
 	var rotWorldMatrix;
+
+	var targetList;
+
+	var ray = new THREE.Raycaster();
 
 	var windowHalfX = window.innerWidth / 2;
 	var windowHalfY = window.innerHeight / 2;
 	var rotationSpeed = 2;
-	var lastMoveTimestamp,
-		moveReleaseTimeDelta = 50;
+	var lastMoveTimestamp = new Date();
+	var moveReleaseTimeDelta = 50;
 
 	var startPoint = {
 		x: 0,
@@ -52,22 +58,45 @@ Defmech.RotationWithQuaternion = (function()
 	var setup = function()
 	{
 		container = document.createElement('div');
+		container.style.display = 'flex';
+		container.style.alignItems = 'center';
+		container.style.height = '800px';
+		container.style.background = 'black';
 		document.body.appendChild(container);
 
 		var info = document.createElement('div');
 		info.style.position = 'absolute';
 		info.style.top = '10px';
-		info.style.width = '100%';
+		info.style.width = '100px';
 		info.style.textAlign = 'center';
 		info.innerHTML = '3D - cube0';
 		container.appendChild(info);
 
+		// var button = document.createElement('div');
+		// button.innerHTML = 'Rotate X'
+		// button.style.border = '1px solid black'
+		// button.style.padding = '10px';
+		// button.style.width = '70px';
+		// button.style.height = '20px';
+		// button.style.borderRadius = '6px';
+		// button.style.cursor = 'pointer';
+		// button.style.className = "button";
+		// button.style.background = 'lightgrey';
+
+
+		// container.appendChild(button);
+
+		// var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+		// var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+		// camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+
 		camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
 		camera.position.y = 150;
-		camera.position.z = 500;
+		camera.position.z = 400;
+
+		// projector = new THREE.Projector();
 
 		scene = new THREE.Scene();
-
 		// Cube
 		var size = 100;
 
@@ -76,11 +105,12 @@ Defmech.RotationWithQuaternion = (function()
 			vertexColors: THREE.FaceColors,
 			overdraw: 1,
 			transparent: true,
-			opacity: 0
+			opacity: 0.1
 		});
 		var white = new THREE.MeshBasicMaterial({
 			overdraw: 1,
-			transparent: false
+			transparent: false,
+			color: 0xffffff
 		});
 		var yellow = new THREE.MeshBasicMaterial({
 			overdraw: 1,
@@ -107,6 +137,7 @@ Defmech.RotationWithQuaternion = (function()
 			transparent: false,
 			color: 0xffb000
 		});
+
 		var cubeMaterial = [transparent, white, yellow, red, green, blue, orange];
 
 		var boxGeometryW1R = new THREE.BoxGeometry(size, size, size, 3, 3, 3);
@@ -124,71 +155,52 @@ Defmech.RotationWithQuaternion = (function()
 			boxGeometryB5F.faces[i].materialIndex = 0;
 			boxGeometryG6Bk.faces[i].materialIndex = 0;
 		}
-		for (var i = 0; i < 108; i += 2)
+		for (var i = 0; i < 108; i += 1)
 		{
 			var color;
 			if(i < 18){
-				color = 0xFF2000;
 				boxGeometryW1R.faces[i].materialIndex = 1;
-				boxGeometryW1R.faces[i + 1].materialIndex = 1;
-				// debugger
-				boxGeometryW1R.faces[i].color.setHex(0xff2000);
-				boxGeometryW1R.faces[i + 1].color.setHex(color);
-
+				// boxGeometryW1R.faces[i + 1].materialIndex = 1;
 			} else if (i < 36){
-				color = 0xFFFfff;
-				boxGeometryY2L.faces[i].materialIndex = 0;
-				boxGeometryY2L.faces[i + 1].materialIndex = 0;
-				boxGeometryY2L.faces[i].color.setHex(color);
-				// boxGeometryY2L.faces[i + 1].color.setHex(color);
-			} else if (i < 54){
-				// color = 0xFFF200;
-				// boxGeometryY2L.faces[i].color.setHex(color);
-				// boxGeometryY2L.faces[i + 1].color.setHex(color);
-			} else if (i < 72){
-				// color = 0xFFF200;
-				// boxGeometryY2L.faces[i].color.setHex(color);
-				// boxGeometryY2L.faces[i + 1].color.setHex(color);
-			} else if (i < 90){
-				// color = 0xFFF200;
-				// boxGeometryY2L.faces[i].color.setHex(color);
-				// boxGeometryY2L.faces[i + 1].color.setHex(color);
-			} else {
+				boxGeometryY2L.faces[i].materialIndex = 2;
+				// boxGeometryY2L.faces[i + 1].materialIndex = 2;
 			}
+			// else if (i < 54){
+			// 	// color = 0xFFF200;
+			// 	// boxGeometryY2L.faces[i].color.setHex(color);
+			// 	// boxGeometryY2L.faces[i + 1].color.setHex(color);
+			// } else if (i < 72){
+			// 	// color = 0xFFF200;
+			// 	// boxGeometryY2L.faces[i].color.setHex(color);
+			// 	// boxGeometryY2L.faces[i + 1].color.setHex(color);
+			// } else if (i < 90){
+			// 	// color = 0xFFF200;
+			// 	// boxGeometryY2L.faces[i].color.setHex(color);
+			// 	// boxGeometryY2L.faces[i + 1].color.setHex(color);
+			// } else {
+			// }
 		}
 
-		var cubeMaterial2 = new THREE.MeshBasicMaterial(
-		{
-			vertexColors: THREE.FaceColors,
-			overdraw: 0.5,
-			opacity: 1,
-			transparent: true
-		});
-
-
-		var light = new THREE.PointLight( 0xfff200, 1, 100 );
-		light.position.set( 50, 50, 50 );
-		scene.add( light );
 
 // CUBES!---------------------------------------------------------------
 		cube0 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
-			// cube1 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube2 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube3 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube4 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube5 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube6 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube7 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube8 = new THREE.Mesh(boxGeometry, cubeMaterial);
+			cube1 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube2 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
+			cube3 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
+			cube4 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
+			cube5 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
+			cube6 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
+			cube7 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
+			cube8 = new THREE.Mesh(boxGeometryW1R, cubeMaterial);
 			cube9 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
-			// cube10 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube11 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube12 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube13 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube14 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube15 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube16 = new THREE.Mesh(boxGeometry, cubeMaterial);
-			// cube17 = new THREE.Mesh(boxGeometry, cubeMaterial);
+			cube10 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube11 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube12 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube13 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube14 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube15 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube16 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
+			cube17 = new THREE.Mesh(boxGeometryY2L, cubeMaterial);
 			// cube18 = new THREE.Mesh(boxGeometry, cubeMaterial);
 			// cube19 = new THREE.Mesh(boxGeometry, cubeMaterial);
 			// cube20 = new THREE.Mesh(boxGeometry, cubeMaterial);
@@ -202,57 +214,57 @@ Defmech.RotationWithQuaternion = (function()
 		cube0.position.x = 0; // (0: -1, -1, -1)
 			cube0.position.y = 0;
 			cube0.position.z = 0;
-			// cube1.position.x = 0; // (1: -1, -1, 0)
-			// cube1.position.y = 0;
-			// cube1.position.z = 0;
-			// cube2.position.x = 0; // (2: -1, -1, 1)
-			// cube2.position.y = 0;
-			// cube2.position.z = 0;
-			// cube3.position.x = 0; // (3: -1, 0, -1)
-			// cube3.position.y = 0;
-			// cube3.position.z = 0;
-			// cube4.position.x = 0; // (4: -1, 0, 0)
-			// cube4.position.y = 0;
-			// cube4.position.z = 0;
-			// cube5.position.x = 0; // (5: -1, 0, 1)
-			// cube5.position.y = 0;
-			// cube5.position.z = 0;
-			// cube6.position.x = 0; // (6: -1, 1, -1)
-			// cube6.position.y = 0;
-			// cube6.position.z = 0;
-			// cube7.position.x = 0; // (7: -1, 1, 0)
-			// cube7.position.y = 0;
-			// cube7.position.z = 0;
-			// cube8.position.x = 0; // (8: -1, 1, 1)
-			// cube8.position.y = 0;
-			// cube8.position.z = 0;
+			cube1.position.x = 0; // (1: -1, -1, 0)
+			cube1.position.y = 0;
+			cube1.position.z = 0;
+			cube2.position.x = 0; // (2: -1, -1, 1)
+			cube2.position.y = 0;
+			cube2.position.z = 0;
+			cube3.position.x = 0; // (3: -1, 0, -1)
+			cube3.position.y = 0;
+			cube3.position.z = 0;
+			cube4.position.x = 0; // (4: -1, 0, 0)
+			cube4.position.y = 0;
+			cube4.position.z = 0;
+			cube5.position.x = 0; // (5: -1, 0, 1)
+			cube5.position.y = 0;
+			cube5.position.z = 0;
+			cube6.position.x = 0; // (6: -1, 1, -1)
+			cube6.position.y = 0;
+			cube6.position.z = 0;
+			cube7.position.x = 0; // (7: -1, 1, 0)
+			cube7.position.y = 0;
+			cube7.position.z = 0;
+			cube8.position.x = 0; // (8: -1, 1, 1)
+			cube8.position.y = 0;
+			cube8.position.z = 0;
 			cube9.position.x = 0; // (9: 0, -1, -1)
 			cube9.position.y = 0;
 			cube9.position.z = 0;
-			// cube10.position.x = 0; // (10: 0, -1, 0)
-			// cube10.position.y = 0;
-			// cube10.position.z = 0;
-			// cube11.position.x = 0; // (11: 0, -1, 1)
-			// cube11.position.y = 0;
-			// cube11.position.z = 0;
-			// cube12.position.x = 0; // (12: 0, 0, -1)
-			// cube12.position.y = 0;
-			// cube12.position.z = 0;
-			// cube13.position.x = 0; // (13: 0, 0, 0)
-			// cube13.position.y = 0;
-			// cube13.position.z = 0;
-			// cube14.position.x = 0; // (14: 0, 0, 1)
-			// cube14.position.y = 0;
-			// cube14.position.z = 0;
-			// cube15.position.x = 0; // (15: 0, 1, -1)
-			// cube15.position.y = 0;
-			// cube15.position.z = 0;
-			// cube16.position.x = 0; // (16: 0, 1, 0)
-			// cube16.position.y = 0;
-			// cube16.position.z = 0;
-			// cube17.position.x = 0; // (17: 0, 1, 1)
-			// cube17.position.y = 0;
-			// cube17.position.z = 0;
+			cube10.position.x = 0; // (10: 0, -1, 0)
+			cube10.position.y = 0;
+			cube10.position.z = 0;
+			cube11.position.x = 0; // (11: 0, -1, 1)
+			cube11.position.y = 0;
+			cube11.position.z = 0;
+			cube12.position.x = 0; // (12: 0, 0, -1)
+			cube12.position.y = 0;
+			cube12.position.z = 0;
+			cube13.position.x = 0; // (13: 0, 0, 0)
+			cube13.position.y = 0;
+			cube13.position.z = 0;
+			cube14.position.x = 0; // (14: 0, 0, 1)
+			cube14.position.y = 0;
+			cube14.position.z = 0;
+			cube15.position.x = 0; // (15: 0, 1, -1)
+			cube15.position.y = 0;
+			cube15.position.z = 0;
+			cube16.position.x = 0; // (16: 0, 1, 0)
+			cube16.position.y = 0;
+			cube16.position.z = 0;
+			cube17.position.x = 0; // (17: 0, 1, 1)
+			cube17.position.y = 0;
+			cube17.position.z = 0;
 			// cube18.position.x = 0; // (18: 1, -1, -1)
 			// cube18.position.y = 0;
 			// cube18.position.z = 0;
@@ -282,7 +294,7 @@ Defmech.RotationWithQuaternion = (function()
 			// cube26.position.z = 0;
 
 		scene.add(cube0)
-			// scene.add(cube1)
+			scene.add(cube1)
 			// scene.add(cube2)
 			// scene.add(cube3)
 			// scene.add(cube4)
@@ -290,7 +302,7 @@ Defmech.RotationWithQuaternion = (function()
 			// scene.add(cube6)
 			// scene.add(cube7)
 			// scene.add(cube8)
-			scene.add(cube9)
+			// scene.add(cube9)
 			// scene.add(cube10)
 			// scene.add(cube11)
 			// scene.add(cube12)
@@ -309,7 +321,9 @@ Defmech.RotationWithQuaternion = (function()
 			// scene.add(cube25)
 			// scene.add(cube26)
 
-console.log('cube0',cube0);
+		targetList = [cube0, cube1
+										];
+
 	// CUBES!---------------------------------------------------------------
 
 	// Plane (shadow)
@@ -330,13 +344,15 @@ console.log('cube0',cube0);
 		renderer.setSize(window.innerWidth, window.innerHeight);
 
 		container.appendChild(renderer.domElement);
-
+		// var canvas = document.getElementsByTagName('canvas')[0];
 		document.addEventListener('mousedown', onDocumentMouseDown, false);
+		// button.addEventListener('click',() => cube0.rotateY(-Math.PI / 2));
+		// document.addEventListener('mousedown', onDocumentMouseDown, false);
 
 		window.addEventListener('resize', onWindowResize, false);
 
 		animate();
-	};
+	}; //end init/setup
 
 function onWindowResize()
 {
@@ -391,6 +407,45 @@ function onDocumentMouseUp(event)
 
 	document.removeEventListener('mousemove', onDocumentMouseMove, false);
 	document.removeEventListener('mouseup', onDocumentMouseUp, false);
+
+	getFace(event);
+}
+
+function getFace(event){
+		console.log("Click.");
+
+		// update the mouse variable
+		// mouse.x = ( event.clientX / (window.innerWidth) ) * 2 - 1;
+		// mouse.y = - ( event.clientY / (window.innerHeight) ) * 2 + 1;
+		mouse.x = ( ( event.clientX - renderer.domElement.offsetLeft ) / renderer.domElement.clientWidth ) * 2 - 1;
+		mouse.y = - ( ( event.clientY - renderer.domElement.offsetTop ) / renderer.domElement.clientHeight ) * 2 + 1;
+		console.log('mx',mouse.x);
+		console.log('my',mouse.y);
+		// find intersections
+
+		// create a Ray with origin at the mouse position
+		//   and direction into the scene (camera direction)
+		var vector = new THREE.Vector3( mouse.x, mouse.y, 0 );
+		vector.unproject(camera);
+
+
+		console.log('mouse', mouse);
+		console.log('cam', camera);
+		// var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+		ray.setFromCamera( mouse, camera);
+
+
+
+		console.log('chillen', scene.children);
+		var intersects = ray.intersectObjects( scene.children );
+		console.log('hello', intersects);
+
+		if ( intersects.length > 0 )
+		{
+			console.log("Hit @ " + intersects[0].faceIndex );
+			intersects[ 0 ].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 );
+			intersects[ 0 ].object.geometry.colorsNeedUpdate = true;
+		}
 }
 
 function projectOnTrackball(touchX, touchY)
@@ -422,13 +477,14 @@ function rotateMatrix(rotateStart, rotateEnd)
 	quaternion = new THREE.Quaternion();
 
 	var angle = Math.acos(rotateStart.dot(rotateEnd) / rotateStart.length() / rotateEnd.length());
-
+	// console.log(cube0);
 	if (angle)
 	{
 		axis.crossVectors(rotateStart, rotateEnd).normalize();
 		angle *= rotationSpeed;
 		quaternion.setFromAxisAngle(axis, angle);
 	}
+	// console.log('quat', quaternion);
 	return quaternion;
 }
 
@@ -474,26 +530,69 @@ function render()
 	renderer.render(scene, camera);
 }
 
-function rotateAroundWorldAxis(object, axis, radians) {
-    rotWorldMatrix = new THREE.Matrix4();
-    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
-    rotWorldMatrix.multiplySelf(object.matrix);        // pre-multiply
-    object.matrix = rotWorldMatrix;
-    object.rotation.getRotationFromMatrix(object.matrix, object.scale);
-};
-
 var handleRotation = function()
 {
+	// console.log("quat",cube0.quaternion);
+	// console.log("norm",cube0.geometry.faces[0].normal);
 	rotateEndPoint = projectOnTrackball(deltaX, deltaY);
-
 	var rotateQuaternion = rotateMatrix(rotateStartPoint, rotateEndPoint);
 	curQuaternion = cube0.quaternion;
 	curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
 	curQuaternion.normalize();
 	cube0.setRotationFromQuaternion(curQuaternion);
-	// cube9.setRotationFromQuaternion(curQuaternion);
-	// rotateAroundWorldAxis(cube9, axis, 2)
 
+	cur1Quaternion = cube1.quaternion;
+	cur1Quaternion.multiplyQuaternions(rotateQuaternion, cur1Quaternion);
+	cur1Quaternion.normalize();
+	cube1.setRotationFromQuaternion(cur1Quaternion);
+		// cube1.setRotationFromQuaternion(curQuaternion);
+		// cube2.setRotationFromQuaternion(curQuaternion);
+		// cube3.setRotationFromQuaternion(curQuaternion);
+		// cube4.setRotationFromQuaternion(curQuaternion);
+		// cube5.setRotationFromQuaternion(curQuaternion);
+		// cube6.setRotationFromQuaternion(curQuaternion);
+		// cube7.setRotationFromQuaternion(curQuaternion);
+		// cube8.setRotationFromQuaternion(curQuaternion);
+		// cube9.setRotationFromQuaternion(curQuaternion);
+		// cube10.setRotationFromQuaternion(curQuaternion);
+		// cube11.setRotationFromQuaternion(curQuaternion);
+		// cube12.setRotationFromQuaternion(curQuaternion);
+		// cube13.setRotationFromQuaternion(curQuaternion);
+		// cube14.setRotationFromQuaternion(curQuaternion);
+		// cube15.setRotationFromQuaternion(curQuaternion);
+		// cube16.setRotationFromQuaternion(curQuaternion);
+		// cube17.setRotationFromQuaternion(curQuaternion);
+	// rotateAroundWorldAxis(cube9, axis, 2)
+	// console.log("af",cube0.position);
+	rotateEndPoint = rotateStartPoint;
+};
+
+var xRotation = function()
+{
+	rotateEndPoint = projectOnTrackball(deltaX, deltaY);
+	var rotateQuaternion = rotateMatrix(rotateStartPoint, rotateEndPoint);
+	curQuaternion = cube0.quaternion;
+	curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
+	curQuaternion.normalize();
+	cube0.setRotationFromQuaternion(curQuaternion);
+		cube1.setRotationFromQuaternion(curQuaternion);
+		cube2.setRotationFromQuaternion(curQuaternion);
+		cube3.setRotationFromQuaternion(curQuaternion);
+		cube4.setRotationFromQuaternion(curQuaternion);
+		cube5.setRotationFromQuaternion(curQuaternion);
+		cube6.setRotationFromQuaternion(curQuaternion);
+		cube7.setRotationFromQuaternion(curQuaternion);
+		cube8.setRotationFromQuaternion(curQuaternion);
+		cube9.setRotationFromQuaternion(curQuaternion);
+		cube10.setRotationFromQuaternion(curQuaternion);
+		cube11.setRotationFromQuaternion(curQuaternion);
+		cube12.setRotationFromQuaternion(curQuaternion);
+		cube13.setRotationFromQuaternion(curQuaternion);
+		cube14.setRotationFromQuaternion(curQuaternion);
+		cube15.setRotationFromQuaternion(curQuaternion);
+		cube16.setRotationFromQuaternion(curQuaternion);
+		cube17.setRotationFromQuaternion(curQuaternion);
+	// rotateAroundWorldAxis(cube9, axis, 2)
 	rotateEndPoint = rotateStartPoint;
 };
 
