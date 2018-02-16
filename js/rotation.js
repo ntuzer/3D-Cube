474c48cd1fb2d414d2035fdc114de7c991223393
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-02-15 21:05:14 -0800

    Create faces State

--- js/rotation.js
+++ js/rotation.js
@@ -14,6 +14,7 @@ Defmech.RotationWithQuaternion = (function() {
 	var cubeState;  //*
 //*******************
 
+	var facesSTATE;
 	var container;
 	var camera, scene, renderer;
 	var cube1, cube2, cube3,
@@ -29,7 +30,34 @@ Defmech.RotationWithQuaternion = (function() {
 	var mouse = new THREE.Vector2();
 	var rotateStartPoint = new THREE.Vector3(0, 0, 1);
 	var rotateEndPoint = new THREE.Vector3(0, 0, 1);
-	var curQuaternion, cur1Quaternion;
+	var cur1Quaternion,
+			cur2Quaternion,
+			cur3Quaternion,
+			cur4Quaternion,
+			cur5Quaternion,
+			cur6Quaternion,
+			cur7Quaternion,
+			cur8Quaternion,
+			cur9Quaternion,
+			cur10Quaternion,
+			cur11Quaternion,
+			cur12Quaternion,
+			cur13Quaternion,
+			cur14Quaternion,
+			cur15Quaternion,
+			cur16Quaternion,
+			cur17Quaternion,
+			cur18Quaternion,
+			cur19Quaternion,
+			cur20Quaternion,
+			cur21Quaternion,
+			cur22Quaternion,
+			cur23Quaternion,
+			cur24Quaternion,
+			cur25Quaternion,
+			cur26Quaternion,
+			cur27Quaternion;
+
 	var ray = new THREE.Raycaster();
 	var windowHalfX = window.innerWidth / 2;
 	var windowHalfY = window.innerHeight / 2;
@@ -44,6 +72,117 @@ Defmech.RotationWithQuaternion = (function() {
 			deltaY = 0;
 
 	var setup = function() {
+
+		facesSTATE = {0: 0,
+									1: 1,
+									2: 2,
+									3: 3,
+									4: 4,
+									5: 5,
+									6: 6,
+									7: 7,
+									8: 8,
+									9: 9,
+									10: 10,
+									11: 11,
+									12: 12,
+									13: 13,
+									14: 14,
+									15: 15,
+									16: 16,
+									17: 17,
+									18: 18,
+									19: 19,
+									20: 20,
+									21: 21,
+									22: 22,
+									23: 23,
+									24: 24,
+									25: 25,
+									26: 26,
+									27: 27,
+									28: 28,
+									29: 29,
+									30: 30,
+									31: 31,
+									32: 32,
+									33: 33,
+									34: 34,
+									35: 35,
+									36: 36,
+									37: 37,
+									38: 38,
+									39: 39,
+									40: 40,
+									41: 41,
+									42: 42,
+									43: 43,
+									44: 44,
+									45: 45,
+									46: 46,
+									47: 47,
+									48: 48,
+									49: 49,
+									50: 50,
+									51: 51,
+									52: 52,
+									53: 53,
+									54: 54,
+									55: 55,
+									56: 56,
+									57: 57,
+									58: 58,
+									59: 59,
+									60: 60,
+									61: 61,
+									62: 62,
+									63: 63,
+									64: 64,
+									65: 65,
+									66: 66,
+									67: 67,
+									68: 68,
+									69: 69,
+									70: 70,
+									71: 71,
+									72: 72,
+									73: 73,
+									74: 74,
+									75: 75,
+									76: 76,
+									77: 77,
+									78: 78,
+									79: 79,
+									80: 80,
+									81: 81,
+									82: 82,
+									83: 83,
+									84: 84,
+									85: 85,
+									86: 86,
+									87: 87,
+									88: 88,
+									89: 89,
+									90: 90,
+									91: 91,
+									92: 92,
+									93: 93,
+									94: 94,
+									95: 95,
+									96: 96,
+									97: 97,
+									98: 98,
+									99: 99,
+									100: 100,
+									101: 101,
+									102: 102,
+									103: 103,
+									104: 104,
+									105: 105,
+									106: 106,
+									107: 107,
+									}
+
 		container = document.createElement('div');
 		container.style.display = 'flex';
 		container.style.alignItems = 'center';
@@ -84,7 +223,7 @@ Defmech.RotationWithQuaternion = (function() {
 			vertexColors: THREE.FaceColors,
 			overdraw: 1,
 			transparent: true,
-			opacity: 0.1
+			opacity: 0.007
 		});
 		var white = new THREE.MeshBasicMaterial({
 			overdraw: 1,
@@ -165,7 +304,7 @@ Defmech.RotationWithQuaternion = (function() {
 		  15:{98:6, 99:6},
 		  16:{74:5, 75:5, 50:3, 51:3},
 		  17:{44:3, 45:3},
-		  18:{38:3, 39:3},
+		  18:{38:3, 39:3, 92:6, 93:6},
 		  19:{34:2, 35:2, 54:4, 55:4, 84:5, 85:5},
 		  20:{32:2, 33:2, 60:4, 61:4},
 		  21:{30:2, 31:2, 66:4, 67:4, 106:6, 107:6},
@@ -241,33 +380,6 @@ Defmech.RotationWithQuaternion = (function() {
 				listBoxes[i].faces[keys[j]].materialIndex = faceColorKey[i][keys[j]];
 			}
 		}
-
-
-		// for (var i = 0; i < 108; i += 1) {
-		// 	var color;
-		// 	if(i < 18){
-		// 		boxGeometryW1R.faces[i].materialIndex = 1;
-		// 		// boxGeometryW1R.faces[i + 1].materialIndex = 1;
-		// 	} else if (i < 36){
-		// 		boxGeometryY2L.faces[i].materialIndex = 2;
-		// 		// boxGeometryY2L.faces[i + 1].materialIndex = 2;
-		// 	}
-		// 	// else if (i < 54){
-		// 	// 	// color = 0xFFF200;
-		// 	// 	// boxGeometryY2L.faces[i].color.setHex(color);
-		// 	// 	// boxGeometryY2L.faces[i + 1].color.setHex(color);
-		// 	// } else if (i < 72){
-		// 	// 	// color = 0xFFF200;
-		// 	// 	// boxGeometryY2L.faces[i].color.setHex(color);
-		// 	// 	// boxGeometryY2L.faces[i + 1].color.setHex(color);
-		// 	// } else if (i < 90){
-		// 	// 	// color = 0xFFF200;
-		// 	// 	// boxGeometryY2L.faces[i].color.setHex(color);
-		// 	// 	// boxGeometryY2L.faces[i + 1].color.setHex(color);
-		// 	// } else {
-		// 	// }
-		// }
-
 
 		// CUBES!---------------------------------------------------------------
 		cube1 = new THREE.Mesh(boxGeometry1, cubeMaterial);
@@ -416,7 +528,7 @@ Defmech.RotationWithQuaternion = (function() {
 		container.appendChild(renderer.domElement);
 		// var canvas = document.getElementsByTagName('canvas')[0];
 		document.addEventListener('mousedown', onDocumentMouseDown, false);
-		button.addEventListener('click',() => cube0.rotateY(-Math.PI / 2));
+		button.addEventListener('click',() => cube1.rotateY(-Math.PI / 2));
 		// window.addEventListener('resize', onWindowResize, false);
 
 		animate();
@@ -599,150 +711,144 @@ function render() {
 }
 
 var handleRotation = function() {
-	// console.log("quat",cube0.quaternion);
-	// console.log("norm",cube0.geometry.faces[0].normal);
 	rotateEndPoint = projectOnTrackball(deltaX, deltaY);
 	var rotateQuaternion = rotateMatrix(rotateStartPoint, rotateEndPoint);
 
-	// curQuaternion = cube1.quaternion;
-	// curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
-	// curQuaternion.normalize();
-	// cube1.setRotationFromQuaternion(curQuaternion);
-
 	cur1Quaternion = cube1.quaternion;
-cur1Quaternion.multiplyQuaternions(rotateQuaternion, cur1Quaternion);
-cur1Quaternion.normalize();
-cube1.setRotationFromQuaternion(cur1Quaternion);
+		cur1Quaternion.multiplyQuaternions(rotateQuaternion, cur1Quaternion);
+		cur1Quaternion.normalize();
+		cube1.setRotationFromQuaternion(cur1Quaternion);
 
-cur2Quaternion = cube2.quaternion;
-cur2Quaternion.multiplyQuaternions(rotateQuaternion, cur2Quaternion);
-cur2Quaternion.normalize();
-cube2.setRotationFromQuaternion(cur2Quaternion);
+		cur2Quaternion = cube2.quaternion;
+		cur2Quaternion.multiplyQuaternions(rotateQuaternion, cur2Quaternion);
+		cur2Quaternion.normalize();
+		cube2.setRotationFromQuaternion(cur2Quaternion);
 
-cur3Quaternion = cube3.quaternion;
-cur3Quaternion.multiplyQuaternions(rotateQuaternion, cur3Quaternion);
-cur3Quaternion.normalize();
-cube3.setRotationFromQuaternion(cur3Quaternion);
+		cur3Quaternion = cube3.quaternion;
+		cur3Quaternion.multiplyQuaternions(rotateQuaternion, cur3Quaternion);
+		cur3Quaternion.normalize();
+		cube3.setRotationFromQuaternion(cur3Quaternion);
 
-cur4Quaternion = cube4.quaternion;
-cur4Quaternion.multiplyQuaternions(rotateQuaternion, cur4Quaternion);
-cur4Quaternion.normalize();
-cube4.setRotationFromQuaternion(cur4Quaternion);
+		cur4Quaternion = cube4.quaternion;
+		cur4Quaternion.multiplyQuaternions(rotateQuaternion, cur4Quaternion);
+		cur4Quaternion.normalize();
+		cube4.setRotationFromQuaternion(cur4Quaternion);
 
-cur5Quaternion = cube5.quaternion;
-cur5Quaternion.multiplyQuaternions(rotateQuaternion, cur5Quaternion);
-cur5Quaternion.normalize();
-cube5.setRotationFromQuaternion(cur5Quaternion);
+		cur5Quaternion = cube5.quaternion;
+		cur5Quaternion.multiplyQuaternions(rotateQuaternion, cur5Quaternion);
+		cur5Quaternion.normalize();
+		cube5.setRotationFromQuaternion(cur5Quaternion);
 
-cur6Quaternion = cube6.quaternion;
-cur6Quaternion.multiplyQuaternions(rotateQuaternion, cur6Quaternion);
-cur6Quaternion.normalize();
-cube6.setRotationFromQuaternion(cur6Quaternion);
+		cur6Quaternion = cube6.quaternion;
+		cur6Quaternion.multiplyQuaternions(rotateQuaternion, cur6Quaternion);
+		cur6Quaternion.normalize();
+		cube6.setRotationFromQuaternion(cur6Quaternion);
 
-cur7Quaternion = cube7.quaternion;
-cur7Quaternion.multiplyQuaternions(rotateQuaternion, cur7Quaternion);
-cur7Quaternion.normalize();
-cube7.setRotationFromQuaternion(cur7Quaternion);
+		cur7Quaternion = cube7.quaternion;
+		cur7Quaternion.multiplyQuaternions(rotateQuaternion, cur7Quaternion);
+		cur7Quaternion.normalize();
+		cube7.setRotationFromQuaternion(cur7Quaternion);
 
-cur8Quaternion = cube8.quaternion;
-cur8Quaternion.multiplyQuaternions(rotateQuaternion, cur8Quaternion);
-cur8Quaternion.normalize();
-cube8.setRotationFromQuaternion(cur8Quaternion);
+		cur8Quaternion = cube8.quaternion;
+		cur8Quaternion.multiplyQuaternions(rotateQuaternion, cur8Quaternion);
+		cur8Quaternion.normalize();
+		cube8.setRotationFromQuaternion(cur8Quaternion);
 
-cur9Quaternion = cube9.quaternion;
-cur9Quaternion.multiplyQuaternions(rotateQuaternion, cur9Quaternion);
-cur9Quaternion.normalize();
-cube9.setRotationFromQuaternion(cur9Quaternion);
+		cur9Quaternion = cube9.quaternion;
+		cur9Quaternion.multiplyQuaternions(rotateQuaternion, cur9Quaternion);
+		cur9Quaternion.normalize();
+		cube9.setRotationFromQuaternion(cur9Quaternion);
 
-cur10Quaternion = cube10.quaternion;
-cur10Quaternion.multiplyQuaternions(rotateQuaternion, cur10Quaternion);
-cur10Quaternion.normalize();
-cube10.setRotationFromQuaternion(cur10Quaternion);
+		cur10Quaternion = cube10.quaternion;
+		cur10Quaternion.multiplyQuaternions(rotateQuaternion, cur10Quaternion);
+		cur10Quaternion.normalize();
+		cube10.setRotationFromQuaternion(cur10Quaternion);
 
-cur11Quaternion = cube11.quaternion;
-cur11Quaternion.multiplyQuaternions(rotateQuaternion, cur11Quaternion);
-cur11Quaternion.normalize();
-cube11.setRotationFromQuaternion(cur11Quaternion);
+		cur11Quaternion = cube11.quaternion;
+		cur11Quaternion.multiplyQuaternions(rotateQuaternion, cur11Quaternion);
+		cur11Quaternion.normalize();
+		cube11.setRotationFromQuaternion(cur11Quaternion);
 
-cur12Quaternion = cube12.quaternion;
-cur12Quaternion.multiplyQuaternions(rotateQuaternion, cur12Quaternion);
-cur12Quaternion.normalize();
-cube12.setRotationFromQuaternion(cur12Quaternion);
+		cur12Quaternion = cube12.quaternion;
+		cur12Quaternion.multiplyQuaternions(rotateQuaternion, cur12Quaternion);
+		cur12Quaternion.normalize();
+		cube12.setRotationFromQuaternion(cur12Quaternion);
 
-cur13Quaternion = cube13.quaternion;
-cur13Quaternion.multiplyQuaternions(rotateQuaternion, cur13Quaternion);
-cur13Quaternion.normalize();
-cube13.setRotationFromQuaternion(cur13Quaternion);
+		cur13Quaternion = cube13.quaternion;
+		cur13Quaternion.multiplyQuaternions(rotateQuaternion, cur13Quaternion);
+		cur13Quaternion.normalize();
+		cube13.setRotationFromQuaternion(cur13Quaternion);
 
-cur14Quaternion = cube14.quaternion;
-cur14Quaternion.multiplyQuaternions(rotateQuaternion, cur14Quaternion);
-cur14Quaternion.normalize();
-cube14.setRotationFromQuaternion(cur14Quaternion);
+		cur14Quaternion = cube14.quaternion;
+		cur14Quaternion.multiplyQuaternions(rotateQuaternion, cur14Quaternion);
+		cur14Quaternion.normalize();
+		cube14.setRotationFromQuaternion(cur14Quaternion);
 
-cur15Quaternion = cube15.quaternion;
-cur15Quaternion.multiplyQuaternions(rotateQuaternion, cur15Quaternion);
-cur15Quaternion.normalize();
-cube15.setRotationFromQuaternion(cur15Quaternion);
+		cur15Quaternion = cube15.quaternion;
+		cur15Quaternion.multiplyQuaternions(rotateQuaternion, cur15Quaternion);
+		cur15Quaternion.normalize();
+		cube15.setRotationFromQuaternion(cur15Quaternion);
 
-cur16Quaternion = cube16.quaternion;
-cur16Quaternion.multiplyQuaternions(rotateQuaternion, cur16Quaternion);
-cur16Quaternion.normalize();
-cube16.setRotationFromQuaternion(cur16Quaternion);
+		cur16Quaternion = cube16.quaternion;
+		cur16Quaternion.multiplyQuaternions(rotateQuaternion, cur16Quaternion);
+		cur16Quaternion.normalize();
+		cube16.setRotationFromQuaternion(cur16Quaternion);
 
-cur17Quaternion = cube17.quaternion;
-cur17Quaternion.multiplyQuaternions(rotateQuaternion, cur17Quaternion);
-cur17Quaternion.normalize();
-cube17.setRotationFromQuaternion(cur17Quaternion);
+		cur17Quaternion = cube17.quaternion;
+		cur17Quaternion.multiplyQuaternions(rotateQuaternion, cur17Quaternion);
+		cur17Quaternion.normalize();
+		cube17.setRotationFromQuaternion(cur17Quaternion);
 
-cur18Quaternion = cube18.quaternion;
-cur18Quaternion.multiplyQuaternions(rotateQuaternion, cur18Quaternion);
-cur18Quaternion.normalize();
-cube18.setRotationFromQuaternion(cur18Quaternion);
+		cur18Quaternion = cube18.quaternion;
+		cur18Quaternion.multiplyQuaternions(rotateQuaternion, cur18Quaternion);
+		cur18Quaternion.normalize();
+		cube18.setRotationFromQuaternion(cur18Quaternion);
 
-cur19Quaternion = cube19.quaternion;
-cur19Quaternion.multiplyQuaternions(rotateQuaternion, cur19Quaternion);
-cur19Quaternion.normalize();
-cube19.setRotationFromQuaternion(cur19Quaternion);
+		cur19Quaternion = cube19.quaternion;
+		cur19Quaternion.multiplyQuaternions(rotateQuaternion, cur19Quaternion);
+		cur19Quaternion.normalize();
+		cube19.setRotationFromQuaternion(cur19Quaternion);
 
-cur20Quaternion = cube20.quaternion;
-cur20Quaternion.multiplyQuaternions(rotateQuaternion, cur20Quaternion);
-cur20Quaternion.normalize();
-cube20.setRotationFromQuaternion(cur20Quaternion);
+		cur20Quaternion = cube20.quaternion;
+		cur20Quaternion.multiplyQuaternions(rotateQuaternion, cur20Quaternion);
+		cur20Quaternion.normalize();
+		cube20.setRotationFromQuaternion(cur20Quaternion);
 
-cur21Quaternion = cube21.quaternion;
-cur21Quaternion.multiplyQuaternions(rotateQuaternion, cur21Quaternion);
-cur21Quaternion.normalize();
-cube21.setRotationFromQuaternion(cur21Quaternion);
+		cur21Quaternion = cube21.quaternion;
+		cur21Quaternion.multiplyQuaternions(rotateQuaternion, cur21Quaternion);
+		cur21Quaternion.normalize();
+		cube21.setRotationFromQuaternion(cur21Quaternion);
 
-cur22Quaternion = cube22.quaternion;
-cur22Quaternion.multiplyQuaternions(rotateQuaternion, cur22Quaternion);
-cur22Quaternion.normalize();
-cube22.setRotationFromQuaternion(cur22Quaternion);
+		cur22Quaternion = cube22.quaternion;
+		cur22Quaternion.multiplyQuaternions(rotateQuaternion, cur22Quaternion);
+		cur22Quaternion.normalize();
+		cube22.setRotationFromQuaternion(cur22Quaternion);
 
-cur23Quaternion = cube23.quaternion;
-cur23Quaternion.multiplyQuaternions(rotateQuaternion, cur23Quaternion);
-cur23Quaternion.normalize();
-cube23.setRotationFromQuaternion(cur23Quaternion);
+		cur23Quaternion = cube23.quaternion;
+		cur23Quaternion.multiplyQuaternions(rotateQuaternion, cur23Quaternion);
+		cur23Quaternion.normalize();
+		cube23.setRotationFromQuaternion(cur23Quaternion);
 
-cur24Quaternion = cube24.quaternion;
-cur24Quaternion.multiplyQuaternions(rotateQuaternion, cur24Quaternion);
-cur24Quaternion.normalize();
-cube24.setRotationFromQuaternion(cur24Quaternion);
+		cur24Quaternion = cube24.quaternion;
+		cur24Quaternion.multiplyQuaternions(rotateQuaternion, cur24Quaternion);
+		cur24Quaternion.normalize();
+		cube24.setRotationFromQuaternion(cur24Quaternion);
 
-cur25Quaternion = cube25.quaternion;
-cur25Quaternion.multiplyQuaternions(rotateQuaternion, cur25Quaternion);
-cur25Quaternion.normalize();
-cube25.setRotationFromQuaternion(cur25Quaternion);
+		cur25Quaternion = cube25.quaternion;
+		cur25Quaternion.multiplyQuaternions(rotateQuaternion, cur25Quaternion);
+		cur25Quaternion.normalize();
+		cube25.setRotationFromQuaternion(cur25Quaternion);
 
-cur26Quaternion = cube26.quaternion;
-cur26Quaternion.multiplyQuaternions(rotateQuaternion, cur26Quaternion);
-cur26Quaternion.normalize();
-cube26.setRotationFromQuaternion(cur26Quaternion);
+		cur26Quaternion = cube26.quaternion;
+		cur26Quaternion.multiplyQuaternions(rotateQuaternion, cur26Quaternion);
+		cur26Quaternion.normalize();
+		cube26.setRotationFromQuaternion(cur26Quaternion);
 
-cur27Quaternion = cube27.quaternion;
-cur27Quaternion.multiplyQuaternions(rotateQuaternion, cur27Quaternion);
-cur27Quaternion.normalize();
-cube27.setRotationFromQuaternion(cur27Quaternion);
+		cur27Quaternion = cube27.quaternion;
+		cur27Quaternion.multiplyQuaternions(rotateQuaternion, cur27Quaternion);
+		cur27Quaternion.normalize();
+		cube27.setRotationFromQuaternion(cur27Quaternion);
+
 	rotateEndPoint = rotateStartPoint;
 };
 
